8079a766204bd6112baa24a5a89b71777845a6a4
Pablo <pabloesanchez1@gmail.com>
2020-01-04 11:54:02 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# Programming Assignment #2
+
+## Description
+Stock Transaction Program
+
+## Instructions
+1. Write a program to ask the user for stock purchase and sale information. Brokerage commission will be 4 percent
+for both purchase and selling of stock. Ask the user for:
+	- the number of shares of stock purchased/sold (we are assuming this will be the same number)
+	- price of stock (per share) when purchased
+	- price of stock (per share) when sold
+	You are to display the following:
+	- amount of money paid for stock (number of shares * price per share when purchased)
+	- amount of commission paid when purchasing stock
+	- amount of money received when selling stock (number of shares * price per share when sold)
+	- amount of commission paid when selling stock
+	- amount “made” by investor (possibly a loss)
+		- (money received – money paid) – (all commissions paid)
+
+## Sample Run
+
+    Enter number of shares: 2000
+    Enter price of stock at purchase: 40
+    Enter price of stock at sale: 42.75
+    
+    -------------------
+    Cost of Stock Purchase: $ 80000
+    Commission Purchase: $ 3200.0
+    Cost of Stock Sale: $ 85500.0## Heading
+    Commission Sale: $ 3420.0
+    -------------------
+    Profit/Loss: $ -1120.0
---
+++ pa2.py
@@ -0,0 +1,33 @@
+﻿# the number of shares of stock purchased/sold (we are assuming this will be the same number)
+numberShares=eval(input("Enter number of shares: "))
+
+# price of stock (per share) when purchased
+pricePurchase=eval(input("Enter price of stock at purchase: "))
+
+# price of stock (per share) when sold
+priceSale=eval(input("Enter price of stock at sale: "))
+
+# amount of money paid for stock (number of shares * price per share when purchased)
+amountPaid=numberShares*pricePurchase
+
+# amount of commission paid when purchasing stock
+commissionPurchase=amountPaid*4/100
+
+# amount of money received when selling stock (number of shares * price per share when sold)
+amountReceived=numberShares*priceSale
+
+# amount of commission paid when selling stock
+commissionSale=amountReceived*4/100
+
+# amount “made” by investor (possibly a loss)
+# (money received – money paid) – (all commissions paid)
+profit=(amountReceived-amountPaid)-(commissionSale+commissionPurchase)
+
+# display the results
+print("                        -------------------")
+print("Cost of Stock Purchase: $",format(amountPaid, '0.2f'))
+print("Commission Purchase:    $",format(commissionPurchase, '0.2f'))
+print("Cost of Stock Sale:     $",format(amountReceived, '0.2f'))
+print("Commission Sale:        $",format(commissionSale, '0.2f'))
+print("                        -------------------")
+print("Profit/Loss:            $",format(profit, '0.2f'))
